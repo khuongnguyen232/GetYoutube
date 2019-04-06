f2ed310f782a6210473d941e99cd8eca83f1c4b0
TuringCom <softwarejobs@turing.com>
2019-04-06 11:01:01 -0700

    add video player and styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import Youtube from '../api/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = {videos: [] }
+  state = {videos: [], selectedVideo : null};
 
   onTermSubmit = async (term) => {
       const response = await Youtube.get('/search', {
@@ -13,14 +14,34 @@ class App extends React.Component {
       }
     });
 
-    this.setState({videos:response.data.items});
+    this.setState(
+      {videos:response.data.items ,
+      selectedVideo:response.data.items[0]
+    });
   };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo:video});
+  };
+
+  componentDidMount() {
+    this.onTermSubmit('Marvel');
+  }
 
   render() {
     return(
           <div className = "ui container">
-            <SearchBar onFormSubmit = {this.onTermSubmit}/>
-            <VideoList videos = {this.state.videos}/>
+            <SearchBar onFormSubmit = {this.onTermSubmit} />
+            <div className="ui grid">
+              <div className = "ui row">
+                <div className = "eleven wide column">
+                  <VideoDetail video = {this.state.selectedVideo} />
+                </div>
+                <div className = "five wide column">
+                  <VideoList onVideoSelect = {this.onVideoSelect} videos = {this.state.videos}/>
+                </div>
+              </div>
+            </div>
           </div>
     )
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import VideoItem from './VideoItem'
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
   const renderList = videos.map((video) => {
-    return <VideoItem key = {video.id.videoId} video ={video} alt ="Youtube Thumbnails" />
+    return <VideoItem key = {video.id.videoId} video={video }onVideoSelect={onVideoSelect} alt ="Youtube Thumbnails" />
   })
   return(
-    <div>{renderList}</div>
+    <div className="ui relaxed divided list">{renderList}</div>
   );
 };
 
