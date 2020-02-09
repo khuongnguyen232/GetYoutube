a7e1fd7c81756652f99d4a5e730c43ac188ad762
Khuong Nguyen <khuongnguyen232@gmail.com>
2020-02-09 08:31:56 -0800

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,5 @@
 import Axios from 'axios';
 import {YB_API} from '../googleKey';
-//usually should not put the code here, but this is just a demo
 const KEY = YB_API;
 
 export default Axios.create({
